8d6a3709ca1afbbcd2a2d8cfdea22da8dfe39b3a
Madalina <madalinaoancea@hotmail.com>
2024-07-03 20:17:42 -0400

    Update loading spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Oval } from "react-loader-spinner";
 
 function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="loading-spinner-container">
+      <Oval
+        visible={true}
+        height="48"
+        width="48"
+        color="#167a92"
+        secondaryColor="#e2ddd6"
+        ariaLabel="oval-loading"
+        strokeWidth="4"
+        strokeWidthSecondary="4"
+      />
+    </div>
+  );
 }
 
 export default Loading;
